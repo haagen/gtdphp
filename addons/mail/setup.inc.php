43a5853b2af11935008d224b8378bf93140817bb
Martin Haagen <martin@mbh.se>
2013-02-19 16:48:37 +0100

    Added mail addon
    Added addon to import Inbox items from a IMAP capable mail server.

---
+++ addons/mail/setup.inc.php
@@ -0,0 +1,9 @@
+<?php
+$events[_GTD_ON_MENU]['*']=array(
+    'link'   => "mail.inc.php",
+    'title'  => 'Import Email',
+    'label'  => 'Import Email',
+    'where'  => 'item.php?type=i',
+    'when'   => 'after',
+    'options'=> ''
+);
